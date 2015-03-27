88660981cd8e0ed9b09b46d24c757dc9a2071e4c
Dane Andersen <dane.andersen@gmail.com>
2015-03-27 11:11:20 -0400

    Question knows if guess matches answer
    both check sanitized string versions and eval in order to check both format and syntax.

--- models/question.rb
+++ models/question.rb
@@ -1,3 +1,10 @@
 class Question < Sequel::Model
-
+  def matches? guess
+    answer = eval text
+    (sanitize(answer.inspect) == sanitize(guess)) && (answer == eval(guess))
+  end
+  
+  def sanitize string
+    string.tr("'",'"').tr(' ','').gsub(',]',']')
+  end
 end
