8c4b482fdda296815bc42caee35a03f4900959d5
Ethan Hedrick <ethan@sneakysemicolon.com>
2015-12-22 23:09:05 -0500

    Fix api response types
    This was returning everything as javascript instead of json. Oops.

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,16 +1,16 @@
 RubyConans::App.controllers :question do
-  get :index, map: '/questions/:language', provides: [:js] do
+  get :index, map: '/questions/:language', provides: [:json] do
     questions = Question.where(language: params[:language].downcase).order(:display_order)
     questions.to_json
   end
 
-  get :with_topic, map: '/questions/:language/:topic', provides: [:js] do
+  get :with_topic, map: '/questions/:language/:topic', provides: [:json] do
     topic_title = params[:topic].downcase
     @questions = Question.where(language: params[:language].downcase).order(:questions__display_order).graph(Topic.where(title: topic_title), {id: :topic_id}, {join_type: :inner})
     render 'question/with_topic'
   end
 
-  post :answer, with: :question_id, provides: [:js] do
+  post :answer, with: :question_id, provides: [:json] do
     @request_payload = JSON.parse request.body.read
     @question_id = params[:question_id]
     record_guess
--- app/controllers/topic.rb
+++ app/controllers/topic.rb
@@ -1,6 +1,6 @@
 RubyConans::App.controllers :topic do
 
-  get :list, map: '/topics', provides: [:js] do
+  get :list, map: '/topics', provides: [:json] do
     Topic.all.sort{ |a,b| a.display_order <=> b.display_order}.to_json
   end
 
